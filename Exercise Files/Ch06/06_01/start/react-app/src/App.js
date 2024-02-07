dfdd227caa7122e97687bacb0016c1992b2a7b5b
jynoes <89066947+jynoes@users.noreply.github.com>
2024-02-07 13:41:36 +0800

    update fetch graphql

--- Exercise Files/Ch06/06_01/start/react-app/src/App.js
+++ Exercise Files/Ch06/06_01/start/react-app/src/App.js
@@ -1,27 +1,56 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function GithubUser({name, id, avatar}) {
+const query = `
+query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}
+`;
+
+const opts = {
+  method: "POST",
+  headers: { "Content-Type": "application/json"},
+  body: JSON.stringify({ query })
+};
+
+function Lift({ name, elevationGain, status}) {
   return (
     <div>
       <h1>{name}</h1>
-      <p>{id}</p>
-      <img src={avatar} height={100} alt={name}/>
+      <p>{elevationGain} {status}</p>
     </div>
-  )
+  );
 }
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    fetch(`https://api.github.com/users/jynoes`)
+    setLoading(true);
+
+    fetch(`https://snowtooth.moonhighway.com/`, opts)
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
 
-  if(data) return <GithubUser name={data.name} id={data.id} avatar={data.avatar_url} />
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!data) return null;
 
-  return <h1>Data</h1>;
+
+  return (<div>
+    {data.data.allLifts.map((lift) => (
+      <Lift name={lift.name} elevationGain={lift.elevationGain} status={lift.status} />
+    ))}
+  </div>);
 }
 
 export default App;
