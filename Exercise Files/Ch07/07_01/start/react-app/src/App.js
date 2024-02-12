4ee23ca5d9c2ad77d6ffc0bdd31667a8105b207c
jynoes <89066947+jynoes@users.noreply.github.com>
2024-02-12 11:42:52 +0800

    update

--- Exercise Files/Ch07/07_01/start/react-app/src/App.js
+++ Exercise Files/Ch07/07_01/start/react-app/src/App.js
@@ -1,18 +1,36 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
-  )
+  );
 }
 
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </nav>
       <h1>About Us</h1>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
     </div>
   )
 }
@@ -20,12 +38,16 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </nav>
       <h1>Contact Us</h1>
     </div>
-  )
+  );
 }
 
 export function App() {
-  return <Home />
+  return <Home />;
 }
-
--- Exercise Files/Ch07/07_01/start/react-app/src/index.js
+++ Exercise Files/Ch07/07_01/start/react-app/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import {App} from "./App";
-import {About, Contact} from "./App"
+import { App } from "./App";
+import { About, Contact, History } from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />}>
+        <Route element={<History />} path="history"/>
+      </Route>
       <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>,
